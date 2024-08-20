ae2e09480e77a4c0e633f0fccabc343917ad9205
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2024-08-20 13:48:23 +0300

    initial commit

---
+++ bookmytrip-service/src/main/java/com/virtuous/bookmytripservice/BookmytripServiceApplication.java
@@ -0,0 +1,13 @@
+package com.virtuous.bookmytripservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmytripServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmytripServiceApplication.class, args);
+	}
+
+}
---
+++ bookmytrip-service/src/test/java/com/virtuous/bookmytripservice/BookmytripServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtuous.bookmytripservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmytripServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
