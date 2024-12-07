3ba97a924e60f867b5f277516890e053fd16dd82
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2024-12-07 15:49:49 +0300

    exception service kafka variables added

--- bookmytrip-exception-service/src/test/java/com/virtuous/bookmytripexceptionservice/BookmytripExceptionServiceApplicationTests.java
+++ bookmytrip-exception-service/src/test/java/com/virtuous/bookmytripexceptionservice/BookmytripExceptionServiceApplicationTests.java
@@ -1,13 +1,9 @@
 package com.virtuous.bookmytripexceptionservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookmytripExceptionServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
